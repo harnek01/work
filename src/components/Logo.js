8ec8ad00f04405a7bb78569170c68f79b5f9b6be
harnek01 <harneksingh51987@gmail.com>
2023-04-08 02:23:31 +0530

    updated mediaQueries

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -4,7 +4,7 @@ import './stylingSheet.css'
 export default function Logo(props) {
   return (
     <div style={{display:'flex', flexDirection:'column' ,gap:'16px', justifyContent:'center', alignItems:'center'}}>
-    <img src={props.logoUrl} alt='' />
+    <img src={props.logoUrl} className='logoImg' alt='' />
     <div className='btn-content' style={{color: '#686868'}}>
       @fixedInvestments
     </div>
--- src/components/Paragraph.js
+++ src/components/Paragraph.js
@@ -3,7 +3,7 @@ import './stylingSheet.css'
 
 export default function Paragraph(props) {
   return (
-    <div className='para'>
+    <div className='para pressPara'>
       {props.content}
     </div>
   )
